a636dda0029ac693f2ad7bf26e9cd195f6ea0ea0
malar <malarvizhi.44@wipro.com>
2022-07-28 09:52:22 +0000

    Support 7.2.1 VES in TCAGEN2
    - Includes modifications for processing Tca Policy as a List
      to support both v5 and v7 policies
    
    Issue-ID: DCAEGEN2-2976
    Signed-off-by: Malarvizhi Paramasivam <malarvizhi.44@wipro.com>
    Change-Id: I29f56048d825710d268e4588f4b5779d5fdb6020

--- dcae-analytics/dcae-analytics-tca-core/src/main/java/org/onap/dcae/analytics/tca/core/util/function/calculation/TcaEventNameFilter.java
+++ dcae-analytics/dcae-analytics-tca-core/src/main/java/org/onap/dcae/analytics/tca/core/util/function/calculation/TcaEventNameFilter.java
@@ -1,6 +1,7 @@
 /*
- * ================================================================================
+ * ==========LICENSE_START=========================================================
  * Copyright (c) 2018 AT&T Intellectual Property. All rights reserved.
+ * Copyright (c) 2022 Wipro Limited Intellectual Property. All rights reserved.
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,6 +22,7 @@ package org.onap.dcae.analytics.tca.core.util.function.calculation;
 
 import java.util.Optional;
 import java.util.Set;
+import java.util.List;
 import java.util.stream.Collectors;
 
 import org.onap.dcae.analytics.model.cef.CommonEventHeader;
@@ -56,17 +58,22 @@ public class TcaEventNameFilter implements TcaCalculationFunction {
 
         // Get CEF Message Event name and Event names in tca policy
         final String cefMessageEventName = eventNameOptional.get();
-        final TcaPolicy tcaPolicy = tcaExecutionContext.getTcaPolicy();
-        final Set<String> policyEventNames = tcaPolicy.getMetricsPerEventName().stream()
+        final List<TcaPolicy> tcaPolicy = tcaExecutionContext.getTcaPolicy();
+        int count = 0;
+        for( TcaPolicy tcaPol : tcaPolicy){
+            final Set<String> policyEventNames = tcaPol.getMetricsPerEventName().stream()
                 .map(MetricsPerEventName::getEventName).collect(Collectors.toSet());
-
-        // Check CEF Message Event name matches any Policy Event names
-        if (!policyEventNames.contains(cefMessageEventName)) {
-            final String earlyTerminationMessage = String.format(
-                    "CEF Message Event name does not match any Policy Event Names. " +
+            // Check CEF Message Event name matches any Policy Event names
+            if (!policyEventNames.contains(cefMessageEventName)) {
+                    count++;
+                    if(count == 2) {
+                         final String earlyTerminationMessage = String.format(
+                            "CEF Message Event name does not match any Policy Event Names. " +
                             "Message EventName: %s, Policy Event Names: %s", cefMessageEventName, policyEventNames);
-            setTerminatingMessage(earlyTerminationMessage, tcaExecutionContext, false);
-            return tcaExecutionContext;
+                         setTerminatingMessage(earlyTerminationMessage, tcaExecutionContext, false);
+                         return tcaExecutionContext;
+                    }
+            }	    
         }
 
         // CEF Messages one of the the Policy Event names
