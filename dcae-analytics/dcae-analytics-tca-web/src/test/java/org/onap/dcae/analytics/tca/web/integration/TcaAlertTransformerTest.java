5cb8f69f2283f66350665185af607558f7b8c9f8
malar <malarvizhi.44@wipro.com>
2022-07-28 09:52:22 +0000

    Support 7.2.1 VES in TCAGEN2
    - Includes modifications for processing Tca Policy as a List
      to support both v5 and v7 policies
    
    Issue-ID: DCAEGEN2-2976
    Signed-off-by: Malarvizhi Paramasivam <malarvizhi.44@wipro.com>
    Change-Id: I29f56048d825710d268e4588f4b5779d5fdb6020

--- dcae-analytics/dcae-analytics-tca-web/src/test/java/org/onap/dcae/analytics/tca/web/integration/TcaAlertTransformerTest.java
+++ dcae-analytics/dcae-analytics-tca-web/src/test/java/org/onap/dcae/analytics/tca/web/integration/TcaAlertTransformerTest.java
@@ -1,8 +1,9 @@
 /*
- * ================================================================================
+ * =============LICENSE_START======================================================
  * Copyright (c) 2020 ChinaMobile. All rights reserved.
+ * Copyright (c) 2022 Wipro Limited Intellectual Property. All rights reserved.
  * ================================================================================
- * Copyright Copyright (c) 2019 IBM
+ * Copyright (c) 2019 IBM
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -54,7 +55,7 @@ public class TcaAlertTransformerTest extends BaseAnalyticsSpringBootIT {
 
     protected static final String TEST_POLICY_JSON_STRING;
     protected static final String TEST_REQUEST_ID = "testRequestId";
-    protected static final TcaPolicy TEST_TCA_POLICY;
+    protected static final List<TcaPolicy> TEST_TCA_POLICY;
 
     static {
 
@@ -94,7 +95,7 @@ public class TcaAlertTransformerTest extends BaseAnalyticsSpringBootIT {
     }
 
     protected GenericTcaExecutionContextBuilder getTestExecutionContextBuilder(
-            final String cefMessage, final TcaPolicy tcaPolicy, final TcaAbatementContext tcaAbatementContext) {
+            final String cefMessage, final List<TcaPolicy> tcaPolicy, final TcaAbatementContext tcaAbatementContext) {
 
         final TcaProcessingContext tcaProcessingContext = new GenericTcaProcessingContext();
         final TcaResultContext tcaResultContext = new GenericTcaResultContext();
